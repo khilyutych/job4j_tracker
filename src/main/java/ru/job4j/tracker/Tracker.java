d0cf92a48a47dd7b8e02d3f568975ef77fb9e5e2
Khilyutych <igorek3296>
2022-03-20 20:49:00 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #27541]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -39,15 +39,30 @@ public class Tracker {
         return Arrays.copyOf(result, counter);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean replace =  false;
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            replace = true;
+        }
+        return replace;
     }
 }
