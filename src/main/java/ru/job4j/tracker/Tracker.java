51b53b46a863149c70e316f67a3da1f5063ee6c5
Khilyutych <igorek3296>
2022-03-20 21:21:39 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #27536]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -63,4 +63,13 @@ public class Tracker {
         }
         return true;
     }
+
+    public boolean delete(int id) {
+        if (indexOf(id) != -1) {
+            System.arraycopy(items, indexOf(id) + 1, items, indexOf(id), size - indexOf(id) - 1);
+        }
+        items[size - 1] = null;
+        size--;
+        return true;
+    }
 }
