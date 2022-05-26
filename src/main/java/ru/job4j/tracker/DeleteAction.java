641a21d0498f13042bb3dcea7c4ea517a34c790f
Khilyutych <igorek3296>
2022-05-26 22:22:38 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780 #27499]

---
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+public class DeleteAction implements UserAction {
+    @Override
+    public String name() {
+        return "Add new Item";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Delete item ===");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
+        } else {
+            System.out.println("Ошибка удаления заявки.");
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/EditeAction.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+public class EditeAction implements UserAction {
+    @Override
+    public String name() {
+        return "Add new Item";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ===");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Заявка изменена успешно.");
+        } else {
+            System.out.println("Ошибка замены заявки.");
+        }
+
+        return true;
+    }
+}
+
